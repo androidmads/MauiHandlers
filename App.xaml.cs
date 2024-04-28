2bd1f6009e088aa99a1cf502a520b6a4a266bbf2
androidmads <mushtaqat3gb@gmail.com>
2024-04-29 01:05:49 +0530

    updated the handlers

---
+++ App.xaml.cs
@@ -0,0 +1,29 @@
+﻿
+#if ANDROID
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
+#endif
+
+namespace MauiHandlers;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("BorderlessEntry", (handler, view) =>
+        {
+            if (view is Entry)
+            {
+#if ANDROID
+                handler.PlatformView.SetBackgroundColor(Microsoft.Maui.Graphics.Colors.Beige.ToAndroid());
+#elif IOS
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+                handler.PlatformView.BorderThickness = new Microsoft.UI.Xaml.Thickness(10);
+#endif
+            }
+        });
+    }
+}
